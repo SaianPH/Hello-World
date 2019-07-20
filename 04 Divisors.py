25302109a789f5b4cfc653241f8daa452bdeb453
SaianPH <48884745+SaianPH@users.noreply.github.com>
2019-07-20 10:42:29 +0800

    Update 04 Divisors.py

--- 04 Divisors.py
+++ 04 Divisors.py
@@ -1,7 +1,10 @@
+# Task:
 # Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
 # (If you don’t know what a divisor is, it is a number that divides evenly into another number.
 # For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
 
+
+#This is my solution. I created a forever loop wherein you can input an integer and get a list of its factors/divisors.
 
 while True:
     number = int(input("\nPlease enter a number: "))
